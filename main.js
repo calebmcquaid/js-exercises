1f1cb3ecdf7327e2cc3281bfac727993ee85353c
caleb <calebmcquaid@gmail.com>
2018-06-25 18:12:00 -0400

    Added files

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+
+
+var pet = {
+  name: "Jade",
+  color: "black",
+  age: 10,
+};
+
+pet.weight = 7.5;
+
+console.log(pet);
+
+var warm = true;
+
+if (warm){
+  console.log("Wear shorts!");
+} else {
+  console.log("Grab a jacket!");
+};
+
+var weight = 30;
+
+if (weight < 50) {
+  console.log("You are tiny!");
+};
